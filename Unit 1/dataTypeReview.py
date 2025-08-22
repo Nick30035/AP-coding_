928ae8c5e56948668c362e3bb94b21e5c2bfcd9d
Nick30035 <nicholas@bbcdayschool.org.in>
2025-08-22 15:09:20 +0000

    unit1Refresher

---
+++ Unit 1/dataTypeReview.py
@@ -0,0 +1,27 @@
+print('Nicholas Saunders')
+
+1.
+# print each of the following data types:
+ #string
+ print("nick")
+
+ #boolean
+ print(2>1)
+ print (True)
+
+ 
+ 
+
+# integer
+print(30,10,50)
+
+# float
+print(3.25)
+print(2.25)
+
+
+2.
+#create and print a list that features 2 of each data type.
+food = ["bread,tomato sauce, toppings"]
+
+print([True,False,10,1000,20.4, "I like oranges",])
